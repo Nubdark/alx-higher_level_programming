1d1182dea102eb7bc8bfb3793bb9840119a52712
Nubdark <hokaidi58@gmail.com>
2023-01-09 04:07:09 -0800

    T1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a square """
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initializing this square class
+        Args: size - represents size of square defined
+        """
+
+        self.__size = size
