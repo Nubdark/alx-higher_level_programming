cc7bfe41b873aac7b023361e5075ca702795dc6a
Nubdark <hokaidi58@gmail.com>
2023-06-19 07:27:00 -0700

    Corrected 103 file

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,45 +1,5 @@
-#include <stdio.h>
 #include <Python.h>
-
-/**
- * print_python_bytes - Prints bytes information
- *
- * @p: Python Object
- * Return: no return
- */
-void print_python_bytes(PyObject *p)
-{
-	char *string;
-	long int size, i, limit;
-
-	printf("[.] bytes object info\n");
-	if (!PyBytes_Check(p))
-	{
-		printf("  [ERROR] Invalid Bytes Object\n");
-		return;
-	}
-
-	size = ((PyVarObject *)(p))->ob_size;
-	string = ((PyBytesObject *)p)->ob_sval;
-
-	printf("  size: %ld\n", size);
-	printf("  trying string: %s\n", string);
-
-	if (size >= 10)
-		limit = 10;
-	else
-		limit = size + 1;
-
-	printf("  first %ld bytes:", limit);
-
-	for (i = 0; i < limit; i++)
-		if (string[i] >= 0)
-			printf(" %02x", string[i]);
-		else
-			printf(" %02x", 256 + string[i]);
-
-	printf("\n");
-}
+#include <stdio.h>
 
 /**
  * print_python_list - Prints list information
@@ -47,24 +7,50 @@ void print_python_bytes(PyObject *p)
  * @p: Python Object
  * Return: no return
  */
-void print_python_list(PyObject *p)
-{
-	long int size, i;
-	PyListObject *list;
-	PyObject *obj;
+void print_python_list(PyObject *p) {
+    if (!PyList_Check(p)) {
+        printf("[ERROR] Invalid Python list object.\n");
+        return;
+    }
 
-	size = ((PyVarObject *)(p))->ob_size;
-	list = (PyListObject *)p;
+    Py_ssize_t size = PyList_Size(p);
+    Py_ssize_t i;
 
-	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", list->allocated);
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
-	for (i = 0; i < size; i++)
-	{
-		obj = ((PyListObject *)p)->ob_item[i];
-		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
-		if (PyBytes_Check(obj))
-			print_python_bytes(obj);
-	}
-}}
+    for (i = 0; i < size; i++) {
+        PyObject *item = PyList_GetItem(p, i);
+        const char *typeName = Py_TYPE(item)->tp_name;
+        printf("Element %ld: %s\n", i, typeName);
+    }
+}
+
+/**
+ * print_python_bytes - Prints bytes information
+ *
+ * @p: Python Object
+ * Return: no return
+ */
+void print_python_bytes(PyObject *p) {
+    if (!PyBytes_Check(p)) {
+        printf("[ERROR] Invalid Python bytes object.\n");
+        return;
+    }
+
+    Py_ssize_t size = PyBytes_Size(p);
+    Py_ssize_t i;
+
+    printf("[*] Python bytes info\n");
+    printf("[*] Size of the Python Bytes = %ld\n", size);
+    printf("Bytes object content: ");
+
+    // Print up to 10 bytes or the size of the bytes object, whichever is smaller
+    Py_ssize_t numBytes = size < 10 ? size : 10;
+
+    for (i = 0; i < numBytes; i++) {
+        printf("%02x", (unsigned char)PyBytes_AS_STRING(p)[i]);
+    }
+    printf("\n");
+}
